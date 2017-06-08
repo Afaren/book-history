9b5b512003f706906b6fe393270737f788d43e69
Afaren <550047450@qq.com>
2017-06-08 15:31:19 +0800

    afaren - save json to file

--- parse.py
+++ parse.py
@@ -5,20 +5,19 @@ import json
 
 
 read_history = open('read_history.html', encoding="GBK")
-
 html = bs4.BeautifulSoup(read_history,"html.parser")
-
 history = []
 
-for tr in html.find_all('table')[1].find_all('tr'):
+with open('history.json', 'w') as fp:
+    for tr in html.find_all('table')[1].find_all('tr'):
+        tds = tr.find_all('td')
+        td = {'number': tds[0].string.strip(),
+              'name':   tds[1].string.strip(),
+              'code': tds[2].string.strip(),
+              'operation': tds[3].string.strip()}
 
-    tds = tr.find_all('td')
+        history.append(td)
 
-    td = {'number': tds[0].string.strip(),
-          'name':   tds[1].string.strip(),
-          'code': tds[2].string.strip(),
-          'operation': tds[3].string.strip()}
+    json.dump(history, fp, indent=2, ensure_ascii=False)
 
-    history.append(td)
 
-print(json.dumps(history, indent=2, ensure_ascii=False))
