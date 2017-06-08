4c57e9a644e4fbf0693d5b3fcb2aaef32ff048b3
Afaren <550047450@qq.com>
2017-06-08 15:13:06 +0800

    afaren - clean up

--- parse.py
+++ parse.py
@@ -6,18 +6,13 @@ import json
 
 read_history = open('read_history.html', encoding="GBK")
 
-
 html = bs4.BeautifulSoup(read_history,"html.parser")
 
 history = []
 
-# print(html.find_all('table')[1].find_all('tr'))
 for tr in html.find_all('table')[1].find_all('tr'):
 
-    # print(tr.find_all('td'))
     tds = tr.find_all('td')
-    # print( tds[0].string + ', '  + tds[1].string + ', ' + tds[2].string + ', ' + tds[3].string)
-    # print("%s, %s, %s, %s" %(tds[0].string.strip() ,tds[1].string.strip(), tds[2].string.strip(), tds[3].string.strip()))
 
     td = {'number': tds[0].string.strip(),
           'name':   tds[1].string.strip(),
@@ -26,20 +21,4 @@ for tr in html.find_all('table')[1].find_all('tr'):
 
     history.append(td)
 
-    # tds_json = json.dumps(tds_dict, indent=2, ensure_ascii=False)
-
-    # print("%s, " %(tds_json))
-    # print(json.dumps(tds_dict, indent=2, ensure_ascii=False))
-
 print(json.dumps(history, indent=2, ensure_ascii=False))
-    
-    
-    # for td in tr.find_all('td'):
-        # content = td.string.strip()
-        # print(content, end=", ")
-        # print(td.string.strip(), end=', ')
-    # print()
-
-# print(html.find_all('table')[1].find_all('tr'))
-
-#print(html)
