6f5f310b99d28bbd6de9a8779387536e35745782
Chongfa <cfachen@thoughtworks.com>
2017-11-13 09:41:34 +0800

    获取借书记录

--- borrowList.js
+++ borrowList.js
@@ -1,12 +1,12 @@
 let history = require("./history")
 
 
-history = history.filter(h => h.operation !== '还书')
+history = history.filter(h => h.operation === '借书')
                  .map(h => h.name)
                  .map(h => h.split('=')[0].trim())
 
 
-// history = Array.from( new Set(history));
+history = Array.from(new Set(history));
 
-
+// console.log(history.length);
 history.forEach(h => console.log(h))
