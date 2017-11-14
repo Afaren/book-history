eba13ae0e463f2103c642b8fc365b2541b732c4b
Chongfa <cfachen@thoughtworks.com>
2017-11-14 13:51:04 +0800

    一小时请求 80 次，每次请求之间随机停 5 秒

--- getTags.js
+++ getTags.js
@@ -1,32 +1,51 @@
 const agent = require('superagent');
 const async = require('async');
+const _ = require('underscore');
+const bookTitleList = require('./book_title_list');
 
-bookTitles =[
-  '编程珠玑',
-  'Java语言程序设计',
-  '怎样解题',
-  '重构与模式',
-  '新概念计算机英语',
-  '自控力',
-  '把时间当作朋友',
-]
+function sleep(milliseconds) {
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 
+function random_sleep(second) {
+  sleep(Math.floor((Math.random() * second) + 1) * 1000)
+}
 
 function requestTags(bookTitle, done) {
+  random_sleep(5);
   agent.get(encodeURI(`https://api.douban.com/v2/book/search?q="${bookTitle}"&count=1`))
        .end((err, res) => {
            if (err) {
              console.log(err);
            } else {
-             const tag = res.body.books[0].tags
+             const tag = res.body.books[0].tags;
              done(null, tag);
            }
          }
        );
 }
 
-async.map(bookTitles, requestTags, (err, tags) => {
-  tags.forEach(tag => {
-    console.log(tag);
-  })
+
+function partition(items, size) {
+  let result = _.groupBy(items, function(item, i) {
+    return Math.floor(i/size);
+  });
+  return _.values(result);
+}
+
+const milliseconds_of_one_hour = 60 * 60 * 1000;
+
+partition(bookTitleList, 80).forEach(subList => {
+  async.map(subList, requestTags, (err, tags) => {
+    tags.forEach(tag => {
+      console.log(tag);
+    })
+  });
+  sleep(milliseconds_of_one_hour);
 })
+
